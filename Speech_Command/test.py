7de258109303e7de63c00dd2bb2e9d7b856686de
leeang6969 <leeang6969.cs05@g2.nctu.edu.tw>
2019-05-18 15:16:54 +0800

    Update test.py and upload result files

--- Speech_Command/test.py
+++ Speech_Command/test.py
@@ -7,9 +7,9 @@ import time
 def main():
 
 	with open('success.csv') as success_csv:
-		success = np.asarray(list(csv.reader(success_csv))).reshape(10, 10).astype(int)
+		success = np.asarray(list(csv.reader(success_csv))).reshape(10, 10).astype(float)
 	with open('iteration.csv') as iteration_csv:
-		iteration = np.asarray(list(csv.reader(iteration_csv))).reshape(10, 10).astype(int)
+		iteration = np.asarray(list(csv.reader(iteration_csv))).reshape(10, 10).astype(float)
 
 	label = np.asarray(["yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"])
 	search_root="/home/leeanghsuan/Desktop/speech_dataset"
@@ -21,7 +21,7 @@ def main():
 				source_label = label[i]
 				search_dir = search_root + "/" + source_label
 				file_list = np.asarray(os.listdir(search_dir))[: 10]
-				for k in range(iteration[i][j], 10):
+				for k in range(int(iteration[i][j]), 10):
 					print("i = {}\tj = {}\tk = {}".format(i, j, k))
 					input_path = search_dir + "/" + file_list[k]
 					target_label = label[j]
@@ -41,8 +41,8 @@ def main():
 						sys.exit(1)
 					# success
 					if exit_code != 0:
-						success[i][j] += 1
-					iteration[i][j] += 1
+						success[i][j] += 1.0
+					iteration[i][j] += 1.0
 					# save from time to time
 					if k == 9:
 						success.tofile('success1.csv',sep=',',format='%d')
@@ -58,12 +58,13 @@ def main():
 	os.remove("iteration.csv")
 	os.rename("success1.csv", "success.csv")
 	os.rename("iteration1.csv", "iteration.csv")
+	for i in range(10):
+		for j in range(10):
+			success[i][j] /= 10.0
+	np.savetxt('accuracy.txt', success, delimiter=',', fmt='%.2f')
+	success.tofile('accuracy1.csv',sep=',',format='%.2f')
+	os.remove("accuracy.csv")
+	os.rename("accuracy1.csv", "accuracy.csv")
 
 if __name__ == "__main__":
 	main()
-	for i in range(10):
-		for j in range(10):
-			success[i][j] /= 10
-	success.tofile('accuracy1.csv',sep=',',format='%d')
-	os.remove("accuracy.csv")
-	os.rename("accuracy1.csv", "accuracy.csv")
