dcf6ca9e7725b7209312cb56d0017fbefb4d7a89
leeang6969 <leeang6969.cs05@g2.nctu.edu.tw>
2019-10-22 00:46:32 +0800

    Support test for v2

--- Speech_Command/generate_audio_v2.py
+++ Speech_Command/generate_audio_v2.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 from util import *
 from attacker import Attacker
 import os
+import sys
 
 def worker(dataQueue, dataQueueLock, mtDNAQueue, mtDNAQueueLock, i, quit, foundit,data,label,outputTensor,targetLabel,newWavPath):
 	print("%d started" % i)
@@ -20,40 +21,43 @@ def worker(dataQueue, dataQueueLock, mtDNAQueue, mtDNAQueueLock, i, quit, foundi
 
 
 if __name__ == '__main__':
-	flags = tf.flags
-	flags.DEFINE_string("wavPath", "0a2b400e_nohash_0.wav", "the source wav path")
-	flags.DEFINE_string("graphPath", "my_frozen_graph.pb", "the attacked Graph path")
-	flags.DEFINE_string("newWavPath", "new_version_para.wav", "the new wav path")
-	flags.DEFINE_string("labelPath", "conv_labels.txt", "label path")
-	flags.DEFINE_string("target", "yes", "the target")
-	FLAGS = flags.FLAGS
-
-	data = load_wav(FLAGS.wavPath)
-	label = load_labels(FLAGS.labelPath)
-	
 	try:
-		targetLabel = label.index(FLAGS.target)
-	except:
-		print("ERROR: The attack target is not in the label list")
+		flags = tf.flags
+		flags.DEFINE_string("wavPath", "0a2b400e_nohash_0.wav", "the source wav path")
+		flags.DEFINE_string("graphPath", "my_frozen_graph.pb", "the attacked Graph path")
+		flags.DEFINE_string("newWavPath", "new_version_para.wav", "the new wav path")
+		flags.DEFINE_string("labelPath", "conv_labels.txt", "label path")
+		flags.DEFINE_string("target", "yes", "the target")
+		FLAGS = flags.FLAGS
 
-	#import graph
-	load_graph(FLAGS.graphPath)
-	
-	outputTensor = tf.get_default_graph().get_tensor_by_name("labels_softmax:0")
+		data = load_wav(FLAGS.wavPath)
+		label = load_labels(FLAGS.labelPath)
+		
+		try:
+			targetLabel = label.index(FLAGS.target)
+		except:
+			print("ERROR: The attack target is not in the label list")
 
-	quit = mp.Event()
-	foundit = mp.Event()
-	dataQueue = mp.Queue()
-	dataQueueLock = mp.Lock()
-	mtDNAQueue = mp.Queue()
-	mtDNAQueueLock = mp.Lock()
+		#import graph
+		load_graph(FLAGS.graphPath)
+		
+		outputTensor = tf.get_default_graph().get_tensor_by_name("labels_softmax:0")
 
-	all_processes = []
-	for i in range(mp.cpu_count()): #for i in range(mp.cpu_count()):
-		p = mp.Process(target=worker, args=(dataQueue, dataQueueLock, mtDNAQueue, mtDNAQueueLock, i, quit, foundit, data, label, outputTensor, targetLabel, FLAGS.newWavPath))
-		p.start()
-		all_processes.append(p)
-	foundit.wait()
-	quit.set()
-	for process in all_processes: 
-		process.terminate() 
+		quit = mp.Event()
+		foundit = mp.Event()
+		dataQueue = mp.Queue()
+		dataQueueLock = mp.Lock()
+		mtDNAQueue = mp.Queue()
+		mtDNAQueueLock = mp.Lock()
+
+		all_processes = []
+		for i in range(4): #for i in range(mp.cpu_count()):
+			p = mp.Process(target=worker, args=(dataQueue, dataQueueLock, mtDNAQueue, mtDNAQueueLock, i, quit, foundit, data, label, outputTensor, targetLabel, FLAGS.newWavPath))
+			p.start()
+			all_processes.append(p)
+		foundit.wait()
+		quit.set()
+		for process in all_processes: 
+			process.terminate() 
+	except KeyboardInterrupt:
+		sys.exit(128)
--- Speech_Command/test.py
+++ Speech_Command/test.py
@@ -5,11 +5,13 @@ import sys
 import time
 
 def main():
-
-	with open('success.csv') as success_csv:
-		success = np.asarray(list(csv.reader(success_csv))).reshape(10, 10).astype(float)
-	with open('iteration.csv') as iteration_csv:
-		iteration = np.asarray(list(csv.reader(iteration_csv))).reshape(10, 10).astype(float)
+	elapsed_time = 0
+	with open('/home/leeanghsuan/Desktop/Speech_Command/success.csv') as success_csv:
+		success = np.asarray(list(csv.reader(success_csv))).reshape(50, 50).astype(float)
+	with open('/home/leeanghsuan/Desktop/Speech_Command/iteration.csv') as iteration_csv:
+		iteration = np.asarray(list(csv.reader(iteration_csv))).reshape(50, 50).astype(float)
+	with open('/home/leeanghsuan/Desktop/Speech_Command/time.txt', "r") as f:
+		elapsed_time = float(f.readline())
 
 	label = np.asarray(["yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"])
 	search_root="/home/leeanghsuan/Desktop/speech_dataset"
@@ -19,51 +21,58 @@ def main():
 			if i != j:
 				source_label = label[i]
 				search_dir = search_root + "/" + source_label
-				file_list = np.asarray(os.listdir(search_dir))[: 10]
-				for k in range(int(iteration[i][j]), 10):
+				file_list = np.asarray(os.listdir(search_dir))[: 50]
+				for k in range(int(iteration[i][j]), 50):
 					print("i = {}\tj = {}\tk = {}".format(i, j, k))
 					input_path = search_dir + "/" + file_list[k]
 					target_label = label[j]
 					ouput_path = "/home/leeanghsuan/Desktop/Speech_Command/" + source_label + "/" + target_label + "/" + str(success[i][j]) + ".wav"
-					command="python3 generate_audio_v1.py --wav_path " + input_path + " --new_wav_path " + ouput_path + " --target " + target_label
+					command="python3 generate_audio_v2.py --wavPath " + input_path + " --newWavPath " + ouput_path + " --target " + target_label
+					start_time = time.time()
 					exit_code = os.system(command)
-					print(exit_code)
+					elapsed_time = elapsed_time + time.time() - start_time
+
 					# KeyboardInterrupt
 					if exit_code == 2:
-						success.tofile('success1.csv',sep=',',format='%d')
-						iteration.tofile('iteration1.csv',sep=',',format='%d')
-						os.remove("success.csv")
-						os.remove("iteration.csv")
-						os.rename("success1.csv", "success.csv")
-						os.rename("iteration1.csv", "iteration.csv")
+						with open('/home/leeanghsuan/Desktop/Speech_Command/time.txt', "w") as f:
+							f.write(str(elapsed_time))
+						success.tofile('/home/leeanghsuan/Desktop/Speech_Command/success1.csv',sep=',',format='%d')
+						iteration.tofile('/home/leeanghsuan/Desktop/Speech_Command/iteration1.csv',sep=',',format='%d')
+						os.remove("/home/leeanghsuan/Desktop/Speech_Command/success.csv")
+						os.remove("/home/leeanghsuan/Desktop/Speech_Command/iteration.csv")
+						os.rename("/home/leeanghsuan/Desktop/Speech_Command/success1.csv", "/home/leeanghsuan/Desktop/Speech_Command/success.csv")
+						os.rename("/home/leeanghsuan/Desktop/Speech_Command/iteration1.csv", "/home/leeanghsuan/Desktop/Speech_Command/iteration.csv")
 						time.sleep(0.5)
 						sys.exit(1)
 					# success
-					if exit_code != 0:
+					if os.path.isfile(ouput_path) != 0:
 						success[i][j] += 1.0
 					iteration[i][j] += 1.0
 					# save from time to time
 					if k == 9:
-						success.tofile('success1.csv',sep=',',format='%d')
-						iteration.tofile('iteration1.csv',sep=',',format='%d')
-						os.remove("success.csv")
-						os.remove("iteration.csv")
-						os.rename("success1.csv", "success.csv")
-						os.rename("iteration1.csv", "iteration.csv")
-
-	success.tofile('success1.csv',sep=',',format='%d')
-	iteration.tofile('iteration1.csv',sep=',',format='%d')
-	os.remove("success.csv")
-	os.remove("iteration.csv")
-	os.rename("success1.csv", "success.csv")
-	os.rename("iteration1.csv", "iteration.csv")
+						with open('/home/leeanghsuan/Desktop/Speech_Command/time.txt', "w") as f:
+							f.write(str(elapsed_time))
+						success.tofile('/home/leeanghsuan/Desktop/Speech_Command/success1.csv',sep=',',format='%d')
+						iteration.tofile('/home/leeanghsuan/Desktop/Speech_Command/iteration1.csv',sep=',',format='%d')
+						os.remove("/home/leeanghsuan/Desktop/Speech_Command/success.csv")
+						os.remove("/home/leeanghsuan/Desktop/Speech_Command/iteration.csv")
+						os.rename("/home/leeanghsuan/Desktop/Speech_Command/success1.csv", "/home/leeanghsuan/Desktop/Speech_Command/success.csv")
+						os.rename("/home/leeanghsuan/Desktop/Speech_Command/iteration1.csv", "/home/leeanghsuan/Desktop/Speech_Command/iteration.csv")
+	with open('/home/leeanghsuan/Desktop/Speech_Command/time.txt', "w") as f:
+		f.write(str(elapsed_time/4500))
+	success.tofile('/home/leeanghsuan/Desktop/Speech_Command/success1.csv',sep=',',format='%d')
+	iteration.tofile('/home/leeanghsuan/Desktop/Speech_Command/iteration1.csv',sep=',',format='%d')
+	os.remove("/home/leeanghsuan/Desktop/Speech_Command/success.csv")
+	os.remove("/home/leeanghsuan/Desktop/Speech_Command/iteration.csv")
+	os.rename("/home/leeanghsuan/Desktop/Speech_Command/success1.csv", "/home/leeanghsuan/Desktop/Speech_Command/success.csv")
+	os.rename("/home/leeanghsuan/Desktop/Speech_Command/iteration1.csv", "/home/leeanghsuan/Desktop/Speech_Command/iteration.csv")
 	for i in range(10):
 		for j in range(10):
 			success[i][j] /= 10.0
-	np.savetxt('accuracy.txt', success, delimiter=',', fmt='%.2f')
-	success.tofile('accuracy1.csv',sep=',',format='%.2f')
-	os.remove("accuracy.csv")
-	os.rename("accuracy1.csv", "accuracy.csv")
+	np.savetxt('/home/leeanghsuan/Desktop/Speech_Command/accuracy.txt', success, delimiter=',', fmt='%.2f')
+	success.tofile('/home/leeanghsuan/Desktop/Speech_Command/accuracy1.csv',sep=',',format='%.2f')
+	os.remove("/home/leeanghsuan/Desktop/Speech_Command/accuracy.csv")
+	os.rename("/home/leeanghsuan/Desktop/Speech_Command/accuracy1.csv", "/home/leeanghsuan/Desktop/Speech_Command/accuracy.csv")
 
 if __name__ == "__main__":
 	main()
